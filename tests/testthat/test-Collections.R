0cc2538876094f02e50ec2157467b0cb2a6c5ba4
Danielle Callan <dcallan@upenn.edu>
2024-02-11 21:15:47 -0500

    draft some tests

--- tests/testthat/test-Collections.R
+++ tests/testthat/test-Collections.R
@@ -4,7 +4,7 @@ test_that("we can create a new Collection", {
     expect_that(collection, is_a("Collection"))
 
     # a manually populated one/ from a data frame
-    collection <- Collection("my collection", data.frame(entity.id = 1, entity.x = 1, entity.y = 2), "entity.id")
+    collection <- Collection("my collection", data.frame(entity.id = 1, entity.collection_x = 1, entity.collection_y = 2), "entity.id")
     expect_that(collection, is_a("Collection"))
 
     # from a file
@@ -15,36 +15,74 @@ test_that("we can create a new Collection", {
 
 test_that("Collection validation works", {
     # empty name fails
+    expect_error(Collection("", data.frame(entity.id = 1, entity.collection_x = 1, entity.collection_y = 2), "entity.id"))
 
     # empty data fails
+    expect_error(Collection("my collection", data.frame(), "entity.id"))
 
     # negative data values fail
+    expect_error(Collection("my collection", data.frame(entity.id = 1, entity.collection_x = -1, entity.collection_y = 2), "entity.id"))
 
     # empty recordIdColumn fails
+    expect_error(Collection("my collection", data.frame(entity.id = 1, entity.collection_x = 1, entity.collection_y = 2), ""))
 
     # recordIdColumn not in data fails
+    expect_error(Collection("my collection", data.frame(entity.id = 1, entity.collection_x = 1, entity.collection_y = 2), "entity.z"))
 
     # ancestorIdColumns not in data fails
+    expect_error(Collection("my collection", data.frame(entity.id = 1, entity.collection_x = 1, entity.collection_y = 2), "entity.id", "entity.z"))
 
-    # all column names start w the same prefix except ancestorIdColumns or fails
+    # all column names start w the same prefix except id columns or fails
+    expect_error(Collection("my collection", data.frame(entity.id = 1, entity.collection_x = 1, entity.y = 2), "entity.id", "ancestor.id"))
 })
 
 test_that("we can make Collections", {
     # an empty one
+    Collections <- Collections()
+    expect_that(Collections, is_a("Collections"))
 
     # from a list of Collections
+    Collections <- Collections(list(Collection("my collection", data.frame(entity.id = 1, entity.collection_x = 1, entity.collection_y = 2), "entity.id")))
+    expect_that(Collections, is_a("Collections"))
 
-    # from a list of data frames
+    Collections <- Collections(Collection("my collection", data.frame(entity.id = 1, entity.collection_x = 1, entity.collection_y = 2), "entity.id"))
+    expect_that(Collections, is_a("Collections"))
 
-    # from a list of files
+    # from a file
+    file1 <- '../../inst/extdata/DiabImmune/DiabImmune_entity_16SRRNAV4Assay.txt'
+    Collections <- Collections(file1)
+    expect_that(Collections, is_a("Collections"))
+    # TODO check that things have reasonable names and id columns
     
     # from a data frame
-    
-    # from a file
+    df <- data.table::fread(file1)
+    Collections <- Collections(df)
+    expect_that(Collections, is_a("Collections"))
+    # TODO check that things have reasonable names and id columns
+
+    # from a list of files
+    file2 <- '../../inst/extdata/DiabImmune/DiabImmune_MetagenomicSequencingAssay.txt'
+    Collections <- Collections(list(file1, file2))
+    expect_that(Collections, is_a("Collections"))
+    # TODO check that things have reasonable names and id columns
+
+    # from a list of data frames
+    df2 <- data.table::fread(file2)
+    Collections <- Collections(list(df, df2))
+    expect_that(Collections, is_a("Collections"))
+    # TODO check that things have reasonable names and id columns
 })
 
 test_that("Collection validation works", {
     # duplicate names fails
+    expect_error(Collections(list(
+        Collection("my collection", data.frame(entity.id = 1, entity.collection_x = 1, entity.collection_y = 2), "entity.id"),
+        Collection("my collection", data.frame(entity.id = 1, entity.collection_x = 1, entity.collection_y = 2), "entity.id")
+    )))
 
     # a collection w different ancestorIdColumns fails
+    expect_error(Collections(list(
+        Collection("my collection", data.frame(entity.id = 1, entity.collection_x = 1, entity.collection_y = 2), "entity.id", "ancestor.y"),
+        Collection("my collection", data.frame(entity.id = 1, entity.collection_x = 1, entity.collection_y = 2), "entity.id", "ancestor.z")
+    )))
 })
--- tests/testthat/test-MbioDataset.R
+++ tests/testthat/test-MbioDataset.R
@@ -1,15 +1,49 @@
 test_that("we can create a new MbioDataset", {
     # an empty one
+    mbioDataset <- MbioDataset()
+    expect_that(mbioDataset, is_a("MbioDataset"))
     
     # a manually populated one
-    
-    # from a data frame of collections and sample metadata
+    mbioDataset <- MbioDataset(
+        Collections("my collection", data.frame(entity.id = 1, entity.collection_x = 1, entity.collection_y = 2), "entity.id"), 
+        SampleMetadata()
+    )
+    expect_that(mbioDataset, is_a("MbioDataset"))   
+
+    # from a Collection object and SampleMetadata
+    mbioDataset <- MbioDataset(
+        Collection("my collection", data.frame(entity.id = 1, entity.collection_x = 1, entity.collection_y = 2), "entity.id"), 
+        SampleMetadata()
+    )
+    expect_that(mbioDataset, is_a("MbioDataset"))
     
     # from a file of collections and sample metadata
+    dataFile1 <- '../../inst/extdata/DiabImmune/DiabImmune_entity_16SRRNAV4Assay.txt'
+    metadataFile1 <- '../../inst/extdata/DiabImmune/DiabImmune_ParticipantRepeatedMeasure.txt'
+    mbioDataset <- MbioDataset(dataFile1, metadataFile1)
+    expect_that(mbioDataset, is_a("MbioDataset"))
+    # TODO check that things have reasonable names and id columns
+
+    # from a data frame of collections and sample metadata
+    df <- data.table::fread(dataFile1)
+    metadata <- data.table::fread(metadataFile1)
+    mbioDataset <- MbioDataset(df, metadata)
+    expect_that(mbioDataset, is_a("MbioDataset"))
+    # TODO check that things have reasonable names and id columns
 
     # from a list of files of collections and sample metadata
+    dataFile2 <- '../../inst/extdata/DiabImmune/DiabImmune_MetagenomicSequencingAssay.txt'
+    metadataFile2 <- '../../inst/extdata/DiabImmune/DiabImmune_Participant.txt'
+    metadataFile3 <- '../../inst/extdata/DiabImmune/DiabImmune_Sample.txt'
+    mbioDataset <- MbioDataset(list(dataFile1, dataFile2), list(metadataFile1, metadataFile2, metadataFile3))
+    expect_that(mbioDataset, is_a("MbioDataset"))
+    # TODO check that things have reasonable names and id columns
 
-    # from a Collection and SampleMetadata
-
-    # from a Collections object and SampleMetadata
+    # from a list of collections and sample metadata data frames
+    df2 <- data.table::fread(dataFile2)
+    metadata2 <- data.table::fread(metadataFile2)
+    metadata3 <- data.table::fread(metadataFile3)
+    mbioDataset <- MbioDataset(list(df, df2), list(metadata, metadata2, metadata3))
+    expect_that(mbioDataset, is_a("MbioDataset"))
+    # TODO check that things have reasonable names and id columns
 })
