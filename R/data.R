af4524730ed1b7135a80bcdcd5eecd298da61b31
Danielle Callan <dcallan@upenn.edu>
2024-02-14 22:47:30 -0500

    add farmm docs

--- R/data.R
+++ R/data.R
@@ -30,3 +30,27 @@
 #' "WGS Normalized number of taxon-specific sequence matches" \cr
 #' @source <https://microbiomedb.org/mbio/app/workspace/analyses/DS_a2f8877e68/new/download>
 "DiabImmune"
+
+#' FARMM
+#'
+#' The Food and Resulting Microbial Metabolites (FARMM) study set out to define the impact of defined diets on the microbiome and metabolome.
+#' Follows 31 healthy human adult volunteers monitored longitudinaly over a 15 day period.
+#' Includes 454 stool samples and sequencing controls; 'shotgun' metagenomic sequencing.
+#'
+#' @format ## `FARMM`
+#' A MbioDataset object with 17 metadata variables, 454 shotgun stool assays and 150 mass spec assays.
+#' It contains the following collections: \cr
+#' "Metabolomics Mass spectrometry assay"                    
+#' "WGS 4th level EC metagenome abundance data" \cr         
+#' "WGS Metagenome enzyme pathway abundance data" \cr          
+#' "WGS Metagenome enzyme pathway coverage data"  \cr        
+#' "WGS Genus" \cr        
+#' "WGS Species" \cr                                           
+#' "WGS Family" \cr                                         
+#' "WGS Order" \cr                                          
+#' "WGS Phylum" \cr                                           
+#' "WGS Class"  \cr                                          
+#' "WGS Kingdom"  \cr                                          
+#' "WGS Normalized number of taxon-specific sequence matches" \cr
+#' @source <https://microbiomedb.org/mbio/app/workspace/analyses/DS_4dfda49064/new/download>
+"FARMM"
