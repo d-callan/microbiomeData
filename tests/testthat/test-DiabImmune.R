8a23a3a768825d90ad2788eea86678d1342fe2a0
Danielle Callan <dcallan@upenn.edu>
2024-04-04 23:16:40 -0400

    add some improved namespacing to tests

--- tests/testthat/test-DiabImmune.R
+++ tests/testthat/test-DiabImmune.R
@@ -3,10 +3,10 @@
 ## we just want to make sure that DiabImmune is
 ## a MbioDataset object and has reasonable collections
 test_that("DiabImmune is sane", {
-    expect_s4_class(DiabImmune, "MbioDataset")
-    expect_true(length(DiabImmune@collections) > 0)
+    expect_s4_class(microbiomeData::DiabImmune, "MbioDataset")
+    expect_true(length(microbiomeData::DiabImmune@collections) > 0)
 
-    genus <- MicrobiomeDB::getCollection(DiabImmune, "16S Genus")
+    genus <- MicrobiomeDB::getCollection(microbiomeData::DiabImmune, "16S Genus")
     expect_s4_class(genus, "Collection")
     expect_true(length(genus@data) > 0)
 })
